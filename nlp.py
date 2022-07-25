9facb1744056098ce812fde18a683d53be4e0b51
Daisy Yu <100946412+DaisyYuuuuu@users.noreply.github.com>
2022-07-25 22:05:14 +0800

    nlp updates

--- nlp.py
+++ nlp.py
@@ -11,7 +11,7 @@ from sklearn.metrics import roc_auc_score, f1_score
 def score_trans(score):
     if score > 3:
         return 1
-    elif score <= 3:
+    elif score < 3:
         return 0
     else:
         return None
@@ -20,24 +20,24 @@ def words_split(data):
     words_df = data.apply(lambda x:' '.join(jieba.cut(x)))
     return words_df
      
-def review_clean(df_dirty):
-    import simplejson
+def review_clean(df):
+    
     # to dict
-    df_dirty['精选评论'] = df_dirty['精选评论'].map(lambda x:simplejson.loads(x))
+    df['精选评论'] = df['精选评论'].map(lambda x:simplejson.loads(x))
     
     # use explode to give each dict in a list a separate row
-    df_dirty = pd.DataFrame(df_dirty.精选评论.explode().reset_index(drop=True))
+    df = pd.DataFrame(df.精选评论.explode().reset_index(drop=True))
 
     # normalize the column of dicts, join back to the remaining dataframe columns, and drop the unneeded column
-    df_dirty = pd.json_normalize(df_dirty.精选评论)
-    df_dirty = df_dirty[['评论内容','用户总分','店铺id']]
+    df = pd.json_normalize(df.精选评论)
+    df = df[['评论内容','用户总分','店铺id']]
 
     # words_split
-    # df_dirty['评论内容'] = df_dirty['评论内容'].apply(lambda x:' '.join(jieba.cut(x)))
+    df['评论内容'] = df['评论内容'].apply(lambda x:' '.join(jieba.cut(x)))
     
-    df_dirty['用户总分'] = pd.to_numeric(df_dirty['用户总分'],errors='coerce')
+    df['用户总分'] = pd.to_numeric(df['用户总分'],errors='coerce')
     
-    return df_dirty
+    return df
 
 
 #Chinese stop words
@@ -45,8 +45,9 @@ infile = open("raw_data/cn_stopwords.txt",encoding='utf-8')
 stopwords_lst = infile.readlines()
 stopwords = [x.strip() for x in stopwords_lst]    
 
-def ceshi(model,strings):
-    strings_fenci = fenci(pd.Series([strings]))
+def pred_score(model,tv,strings):
+    
+    strings_fenci = words_split(pd.Series([strings]))
     return round(float(model.predict_proba(tv.transform(strings_fenci))[:,1]),2)
     
 def nlp_score(path): 
@@ -58,13 +59,26 @@ def nlp_score(path):
     
     x_train, x_test, y_train, y_test = train_test_split(df_model['评论内容'], df_model['target'], random_state=3, test_size=0.25)
     
+    # 5 times of negative comments
+    index_tmp = y_train==0
+    y_tmp = y_train[index_tmp]
+    x_tmp = x_train[index_tmp]
+    x_train2 = pd.concat([x_train,x_tmp,x_tmp,x_tmp,x_tmp,x_tmp])
+    y_train2 = pd.concat([y_train,y_tmp,y_tmp,y_tmp,y_tmp,y_tmp])
+    
     tv = TfidfVectorizer(stop_words=stopwords, max_features=3000, ngram_range=(1,2))
     tv.fit(x_train)
     
     classifier = MultinomialNB()
-    classifier.fit(tv.transform(x_train), y_train)
+    classifier.fit(tv.transform(x_train2), y_train2)
     
-    df_review['nlp_score'] = df_review['评论内容'].map(lambda x:round(float(classifier.predict_proba([tv.transform([x])[:,1]]),2)))
+    df_review['nlp_score'] = df_review['评论内容'].map(lambda x:pred_score(classifier,tv,x))
+    
+    y_pred = classifier.predict_proba(tv.transform(x_test))[:,1]
+    print(roc_auc_score(y_test,y_pred))
+    
+    # df_review = df_review.groupby(['店铺id'],as_index=False).mean()
+    # df_review = df_review.drop(columns = ['target'])
     
     return df_review
 
