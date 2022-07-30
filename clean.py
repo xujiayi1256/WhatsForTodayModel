669bb9eedcdbef2827cba49714067fce513d6c62
Jiayi Xu <xujiayi_1256@me.com>
2022-07-30 17:27:03 +0800

    update

--- clean.py
+++ clean.py
@@ -1,9 +1,14 @@
-import matplotlib.pyplot as plt
 import os
 import pandas as pd
-import numpy as np
 import simplejson
-from nlp import nlp_score,review_clean,score_trans,pred_score
+from nlp import nlp_score
+import os
+
+import pandas as pd
+import simplejson
+
+from nlp import nlp_score
+
 
 def split_score(df):
     x0 = []
@@ -14,13 +19,14 @@ def split_score(df):
         x0.append(tmp['口味'])
         x1.append(tmp['环境'])
         x2.append(tmp['服务'])
-    test = pd.DataFrame({'口味':x0,'环境':x1, '服务':x2})
-    dd1 = pd.concat([df,test],axis=1)
-    dd1 = dd1.drop(['_id','优惠券信息','其他信息','详情链接','图片链接','店铺电话','店铺均分','评论总数','推荐菜'],axis=1)
-    dd1["口味"] = pd.to_numeric(dd1["口味"],errors='coerce')
-    dd1["环境"] = pd.to_numeric(dd1["环境"],errors='coerce')
-    dd1["服务"] = pd.to_numeric(dd1["服务"],errors='coerce')
+    test = pd.DataFrame({'口味': x0, '环境': x1, '服务': x2})
+    dd1 = pd.concat([df, test], axis=1)
+    dd1 = dd1.drop(['_id', '优惠券信息', '其他信息', '详情链接', '图片链接', '店铺电话', '店铺均分', '评论总数', '推荐菜'], axis=1)
+    dd1["口味"] = pd.to_numeric(dd1["口味"], errors='coerce')
+    dd1["环境"] = pd.to_numeric(dd1["环境"], errors='coerce')
+    dd1["服务"] = pd.to_numeric(dd1["服务"], errors='coerce')
     return dd1
+
 
 def split_dishes(df):
     x0 = []
@@ -31,10 +37,11 @@ def split_dishes(df):
         x0.append(tmp[0])
         x1.append(tmp[1])
         x2.append(tmp[2])
-    test = pd.DataFrame({'推荐菜1':x0,'推荐菜2':x1,'推荐菜3':x2})
-    dd2 = pd.concat([df,test],axis=1)
-    dd2 = dd2.drop(['推荐菜'],axis=1)
+    test = pd.DataFrame({'推荐菜1': x0, '推荐菜2': x1, '推荐菜3': x2})
+    dd2 = pd.concat([df, test], axis=1)
+    dd2 = dd2.drop(['推荐菜'], axis=1)
     return dd2
+
 
 def no_queues(x):
     for i in x:
@@ -43,40 +50,47 @@ def no_queues(x):
         else:
             0
 
+
 def split_review(df):
-    df["评论摘要"] = df["评论摘要"].map(lambda x:simplejson.loads(x))
+    df["评论摘要"] = df["评论摘要"].map(lambda x: simplejson.loads(x))
     df["不用排队"] = df["评论摘要"].apply(no_queues)
     df["不用排队"] = df["不用排队"].fillna(value=0)
-    df = df.drop(columns=['_id','精选评论','评论摘要','带图评论个数'])
-    return df 
-
-def chn_to_eng(df):
-    return df.rename(columns={"人均价格": "price_per_person", "店铺id": "restaurant_id", "店铺名":"restaurant"
-                  ,"店铺地址":"address","店铺总分":"total_review_score","标签1":"tag1","标签2":"tag2"
-                  ,"评论总数":"total_review_number","口味":"taste","环境":"surroundings","服务":"service"
-                  ,"推荐菜1":"recommendation_1","推荐菜2":"recommendation_2","推荐菜3":"recommendation_3"
-                  ,"中评个数":"medium_review_number","好评个数":"good_review_number","差评个数":"bad_review_number"
-                  ,"不用排队":"no_queues"})
-
-def eng_to_chn(df):
-    return df.rename(columns={"price_per_person":"人均价格", "restaurant_id":"店铺id", "restaurant":"店铺名"
-                  ,"address":"店铺地址","total_review_score":"店铺总分","tag1":"标签1","tag2":"标签2"
-                  ,"total_review_number":"评论总数","taste":"口味","surroundings":"环境","service":"服务"
-                  ,"recommendation_1":"推荐菜1","recommendation_2":"推荐菜2","recommendation_3":"推荐菜3"
-                  ,"medium_review_number":"中评个数","good_review_number":"好评个数","bad_review_number":"差评个数"
-                  ,"no_queues":"不用排队"})
-
-def run(path1,path2):
-    df_info = pd.read_csv(os.path.join(os.getcwd(),'raw_data',path1))
-    df_review = pd.read_csv(os.path.join(os.getcwd(),'raw_data',path2))
-    # df = pd.merge(split_dishes(split_score(df_info)),split_review(df_review))
-    df = pd.merge(split_score(df_info),split_review(df_review),on="店铺id")
-    df = pd.merge(df,nlp_score(df_review),on="店铺id")
-    df = df.drop(columns=["推荐菜","用户总分"])
-    df = chn_to_eng(df)
-    df["no_queues%"] = df["no_queues"]/df["total_review_number"]
+    df = df.drop(columns=['_id', '精选评论', '评论摘要', '带图评论个数'])
     return df
 
+
+def chn_to_eng(df):
+    return df.rename(
+        columns={"人均价格": "price_per_person", "店铺id": "restaurant_id", "店铺名": "restaurant"
+            , "店铺地址": "address", "店铺总分": "total_review_score", "标签1": "tag1", "标签2": "tag2"
+            , "评论总数": "total_review_number", "口味": "taste", "环境": "surroundings", "服务": "service"
+            , "推荐菜1": "recommendation_1", "推荐菜2": "recommendation_2", "推荐菜3": "recommendation_3"
+            , "中评个数": "medium_review_number", "好评个数": "good_review_number",
+                 "差评个数": "bad_review_number"
+            , "不用排队": "no_queues"})
+
+
+def eng_to_chn(df):
+    return df.rename(
+        columns={"price_per_person": "人均价格", "restaurant_id": "店铺id", "restaurant": "店铺名"
+            , "address": "店铺地址", "total_review_score": "店铺总分", "tag1": "标签1", "tag2": "标签2"
+            , "total_review_number": "评论总数", "taste": "口味", "surroundings": "环境", "service": "服务"
+            , "recommendation_1": "推荐菜1", "recommendation_2": "推荐菜2", "recommendation_3": "推荐菜3"
+            , "medium_review_number": "中评个数", "good_review_number": "好评个数",
+                 "bad_review_number": "差评个数"
+            , "no_queues": "不用排队"})
+
+
+def run(path1, path2):
+    df_info = pd.read_csv(os.path.join(os.getcwd(), 'raw_data', path1))
+    df_review = pd.read_csv(os.path.join(os.getcwd(), 'raw_data', path2))
+    # df = pd.merge(split_dishes(split_score(df_info)),split_review(df_review))
+    df = pd.merge(split_score(df_info), split_review(df_review), on="店铺id")
+    df = pd.merge(df, nlp_score(df_review), on="店铺id")
+    df = df.drop(columns=["推荐菜", "用户总分"])
+    df = chn_to_eng(df)
+    df["no_queues%"] = df["no_queues"] / df["total_review_number"]
+    return df
 
 
 def recommendation(cuisine, district):
@@ -84,11 +98,11 @@ def recommendation(cuisine, district):
     if district == "全部" and cuisine == "全部":
         df = df
     elif district == "全部":
-        df = df[(df.tag1==cuisine)]
+        df = df[(df.tag1 == cuisine)]
     elif cuisine == "全部":
-        df = df[(df.tag2==district)]
+        df = df[(df.tag2 == district)]
     else:
-        df = df[(df.tag1==cuisine)&(df.tag2==district)]
+        df = df[(df.tag1 == cuisine) & (df.tag2 == district)]
     # df = df[df.nlp_score == df.nlp_score.max()]
     df = df.nlargest(n=3, columns=['nlp_score']).reset_index(drop=True)
     df = df.iloc[:, 1:]
--- nlp.py
+++ nlp.py
@@ -1,11 +1,10 @@
-import pandas as pd
 import jieba
-from matplotlib import pyplot as plt
+import pandas as pd
 import simplejson
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import roc_auc_score, f1_score
 
 
 def score_trans(score):
@@ -16,73 +15,70 @@ def score_trans(score):
     else:
         return None
 
+
 def words_split(data):
-    words_df = data.apply(lambda x:' '.join(jieba.cut(x)))
+    words_df = data.apply(lambda x: ' '.join(jieba.cut(x)))
     return words_df
-     
+
+
 def review_clean(df):
-    
     # to dict
-    df['精选评论'] = df['精选评论'].map(lambda x:simplejson.loads(x))
-    
+    df['精选评论'] = df['精选评论'].map(lambda x: simplejson.loads(x))
+
     # use explode to give each dict in a list a separate row
     df = pd.DataFrame(df.精选评论.explode().reset_index(drop=True))
 
     # normalize the column of dicts, join back to the remaining dataframe columns, and drop the unneeded column
     df = pd.json_normalize(df.精选评论)
-    df = df[['评论内容','用户总分','店铺id']]
+    df = df[['评论内容', '用户总分', '店铺id']]
 
     # words_split
-    df['评论内容'] = df['评论内容'].apply(lambda x:' '.join(jieba.cut(x)))
-    
-    df['用户总分'] = pd.to_numeric(df['用户总分'],errors='coerce')
-    
+    df['评论内容'] = df['评论内容'].apply(lambda x: ' '.join(jieba.cut(x)))
+
+    df['用户总分'] = pd.to_numeric(df['用户总分'], errors='coerce')
+
     return df
 
 
-#Chinese stop words
-infile = open("raw_data/cn_stopwords.txt",encoding='utf-8')
-stopwords_lst = infile.readlines()
-stopwords = [x.strip() for x in stopwords_lst]    
-
-def pred_score(model,tv,strings):
-    
+def pred_score(model, tv, strings):
     strings_fenci = words_split(pd.Series([strings]))
-    return round(float(model.predict_proba(tv.transform(strings_fenci))[:,1]),2)
-    
-def nlp_score(df): 
+    return round(float(model.predict_proba(tv.transform(strings_fenci))[:, 1]), 2)
+
+
+def nlp_score(df):
+    # Chinese stop words
+    infile = open("raw_data/cn_stopwords.txt", encoding='utf-8')
+    stopwords_lst = infile.readlines()
+    stopwords = [x.strip() for x in stopwords_lst]
+
     # df = pd.read_csv(path,encoding='utf-8')
     df_nlp = review_clean(df)
-    
-    df_nlp['target'] = df_nlp['用户总分'].map(lambda x:score_trans(x))  
+
+    df_nlp['target'] = df_nlp['用户总分'].map(lambda x: score_trans(x))
     df_model = df_nlp.dropna()
-    
-    x_train, x_test, y_train, y_test = train_test_split(df_model['评论内容'], df_model['target'], random_state=3, test_size=0.25)
-    
+
+    x_train, x_test, y_train, y_test = train_test_split(df_model['评论内容'], df_model['target'],
+                                                        random_state=3, test_size=0.25)
+
     # 5 times of negative comments
-    index_tmp = y_train==0
+    index_tmp = y_train == 0
     y_tmp = y_train[index_tmp]
     x_tmp = x_train[index_tmp]
-    x_train2 = pd.concat([x_train,x_tmp,x_tmp,x_tmp,x_tmp,x_tmp])
-    y_train2 = pd.concat([y_train,y_tmp,y_tmp,y_tmp,y_tmp,y_tmp])
-    
-    tv = TfidfVectorizer(stop_words=stopwords, max_features=3000, ngram_range=(1,2))
+    x_train2 = pd.concat([x_train, x_tmp, x_tmp, x_tmp, x_tmp, x_tmp])
+    y_train2 = pd.concat([y_train, y_tmp, y_tmp, y_tmp, y_tmp, y_tmp])
+
+    tv = TfidfVectorizer(stop_words=stopwords, max_features=3000, ngram_range=(1, 2))
     tv.fit(x_train)
-    
+
     classifier = MultinomialNB()
     classifier.fit(tv.transform(x_train2), y_train2)
-    
-    df_nlp['nlp_score'] = df_nlp['评论内容'].map(lambda x:pred_score(classifier,tv,x))
-    
-    y_pred = classifier.predict_proba(tv.transform(x_test))[:,1]
-    print(roc_auc_score(y_test,y_pred))
-    
-    df_nlp = df_nlp.groupby(['店铺id'],as_index=False).mean()
-    df_nlp = df_nlp.drop(columns = ['target'])
-    
-    return df_nlp
 
-   
-    
-    
-    
+    df_nlp['nlp_score'] = df_nlp['评论内容'].map(lambda x: pred_score(classifier, tv, x))
+
+    y_pred = classifier.predict_proba(tv.transform(x_test))[:, 1]
+    print(roc_auc_score(y_test, y_pred))
+
+    df_nlp = df_nlp.groupby(['店铺id'], as_index=False).mean()
+    df_nlp = df_nlp.drop(columns=['target'])
+
+    return df_nlp
